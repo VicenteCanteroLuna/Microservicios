2bc47d0dfe1df912fa173178f9c6521a1da32861
Vicente Cantero Luna <vicente.cantero@bosonit.com>
2022-09-02 08:45:00 +0200

    feign

--- services-shopping/src/main/java/com/bosonit/servicesshopping/client/CustomerClient.java
+++ services-shopping/src/main/java/com/bosonit/servicesshopping/client/CustomerClient.java
@@ -1,11 +1,12 @@
 package com.bosonit.servicesshopping.client;
 
 import com.bosonit.servicesshopping.model.Customer;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
+@FeignClient(name = "services-customer", fallback = CustomerHystrixFallbackFactory.class)
 public interface CustomerClient {
     @GetMapping(value = "/customers/{id}")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
--- services-shopping/src/main/java/com/bosonit/servicesshopping/client/ProductClient.java
+++ services-shopping/src/main/java/com/bosonit/servicesshopping/client/ProductClient.java
@@ -1,13 +1,14 @@
 package com.bosonit.servicesshopping.client;
 
 import com.bosonit.servicesshopping.model.Product;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@FeignClient(name = "product-service")
+@FeignClient(name = "service-products")
 @RequestMapping(value = "/products")
 public interface ProductClient {
 
